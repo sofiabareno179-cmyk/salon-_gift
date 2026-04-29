b54a49f4b2457b7d42a2580c6d4b82be137c78bd
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-04-29 16:06:02 -0500

    Update perfil.py

--- salon gift/app/models/perfil.py
+++ salon gift/app/models/perfil.py
@@ -1,15 +1,14 @@
 from app import db
 
 class Perfil(db.Model):
-    __tablename__ = 'perfiles'
+    __tablename__ = 'perfil'
 
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     apellido = db.Column(db.String(50), nullable=True)
     bio = db.Column(db.Text, nullable=True)
     
-    # Llave foránea que conecta con el modelo User
-    idusuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False, unique=True)
+    idusuario = db.Column(db.Integer, db.ForeignKey('usuario.idusuario'), nullable=False, unique=True)
 
     def __init__(self, nombre, apellido, bio, idusuario):
         self.nombre = nombre
