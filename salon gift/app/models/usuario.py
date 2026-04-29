7097d030c84652ecf0c343d07ff8b86788c346e2
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-04-29 13:46:35 -0500

    Update usuario.py

--- salon gift/app/models/usuario.py
+++ salon gift/app/models/usuario.py
@@ -1,6 +1,5 @@
 from flask_login import UserMixin
 from app import db
-from datetime import datetime
 from io import BytesIO
 import base64
 import os
@@ -8,28 +7,32 @@ from flask import url_for, current_app
 from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model, UserMixin): 
     __tablename__='usuario'
-    idusuario=db.Column(db.Integer,primary_key=True)
-    nombreuser=db.Column(db.String(100),unique=True,nullable=False)
-    email=db.Column(db.String(100),unique=True,nullable=False)
-    password_hash=db.Column(db.String(250),nullable=False)
-    telefono=db.Column(db.String(20), nullable=True)
-    def __init__(self, nombreuser, email, password,telefono):
-        self.nombreuser = nombreuser
-        self.email = email
-        self.telefono = telefono
-        self.set_password(password)
+    idusuario = db.Column(db.Integer, primary_key=True)
+    nombreuser = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
+    telefono = db.Column(db.String(20), nullable=True)
+    rol = db.Column(db.String(20), nullable=False, default='cliente')
+
+    def __repr__(self):
+        return f'<Usuario {self.nombreuser} - Rol {self.rol}>' # Corregido
+
     def get_id(self):
         return str(self.idusuario)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
     def check_password(self, password):
-        return check_password_hash(self.password_hash,password)
+        return check_password_hash(self.password_hash, password)
+
     def to_dict(self):
         return {
-            "idsuario": self.idusuario,
+            "idusuario": self.idusuario,
             "nombreuser": self.nombreuser,
             "email": self.email,
-            "telefono":self.telefono
+            "telefono": self.telefono,
+            "rol": self.rol
         }
     def save(self):
         db.session.add(self)
