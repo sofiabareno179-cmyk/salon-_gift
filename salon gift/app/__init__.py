4d4d3aae210925757b2a61485b39094147f50ad4
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-05-04 17:06:30 -0500

    Update __init__.py

--- salon gift/app/__init__.py
+++ salon gift/app/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     from app.routes import (
         auth,agenda_route,citas_route,inventario_route,
         producto_route,proveedores_route,recordatorios_route,
-        usuarios_route, servicios_route
+        usuarios_route, servicios_route, perfil_route
     
     )
     app.register_blueprint(auth.bp)
@@ -34,6 +34,7 @@ def create_app():
     app.register_blueprint(recordatorios_route.bp)
     app.register_blueprint(usuarios_route.bp)
     app.register_blueprint(servicios_route.bp)
+    app.register_blueprint(perfil_route.bp)
 
 
     @app.errorhandler(Exception)
