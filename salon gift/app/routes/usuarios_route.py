b9cb164f6221dea4a9ec4fa2e3dc8d8fb9eb2abf
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-05-04 16:29:05 -0500

    Update usuarios_route.py

--- salon gift/app/routes/usuarios_route.py
+++ salon gift/app/routes/usuarios_route.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify, flash
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify, send_file,flash
 from app.models.usuario import User
 from flask_login import current_user, login_required
 from functools import wraps
 from flask import abort
 from app import db
+from io import BytesIO
+import base64
+import json
 
-# Definimos el blueprint con el nombre 'user'
 bp = Blueprint('user', __name__, url_prefix='/User')
 @bp.route('/')
 def home():
@@ -14,7 +16,7 @@ def home():
 @bp.route('/index')
 def index():
     data = User.query.all()
-    return render_template('index.html', data=data)
+    return render_template('cliente.html', data=data)
 @bp.route('/inicio')
 def admin_required(f):
     @wraps(f)
@@ -41,14 +43,14 @@ def add():
     if request.method == 'POST':
         nombreuser = request.form['nombreuser']
         password = request.form['password'] 
-        email = request.form.get('email')   
-        telefono = request.form.get('telefono')
-        
+        email=request.form.get('email')   
+        telefono=request.form.get('telefono')
         if User.query.filter_by(email=email).first():
             flash(f"El correo {email} ya está registrado.", "danger")
             return redirect(url_for('user.add'))    
         try:
-            new_user = User(nombreuser=nombreuser, email=email, password=password, telefono=telefono)
+            new_user = User(nombreuser=nombreuser, email=email,password=password,telefono=telefono)
+            new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
             flash(f"Usuario {nombreuser} creado con éxito.", "success")
@@ -56,29 +58,21 @@ def add():
         except Exception as e:
            db.session.rollback()
            flash(f"Error al crear usuario: {str(e)}", "danger")
-        return redirect(url_for('user.add'))
+       
     return render_template('users/add.html')
 
 @bp.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    # SQLAlchemy usa automáticamente la primary key (idusuario) con .get_or_404()
-    user = User.query.get_or_404(id) 
+    user = User.query.get_or_404(id)
     if request.method == 'POST':
-        # Usamos los nombres exactos de tu modelo User
-        user.nombreuser = request.form['nombreuser'] 
+        user.nameUser = request.form['nameUser']
         user.email = request.form['email']
-        user.telefono = request.form['telefono']
-        
-        # Si el formulario envía una nueva contraseña, la actualizamos
-        if request.form.get('password'):
-            user.set_password(request.form['password'])
-            
+        user.telefono=request.form['telefono']
+        user.passwordUser = request.form['passwordUser']
         db.session.commit()        
-        flash("Usuario actualizado con éxito", "success")
-        return redirect(url_for('user.index'))
+        return redirect(url_for('users/index'))
 
     return render_template('users/edit.html', user=user)
-
 @bp.route('/detail/<int:id>')
 def detail(id):
     user = User.query.get_or_404(id)
@@ -89,5 +83,4 @@ def delete(id):
     user = User.query.get_or_404(id)    
     db.session.delete(user)
     db.session.commit()
-    flash("Usuario eliminado", "warning")
     return redirect(url_for('user.index'))
