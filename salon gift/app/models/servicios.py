c670a4d70371e51c8681efb1a579429df249dad3
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-05-06 17:00:32 -0500

    Update servicios.py

--- salon gift/app/models/servicios.py
+++ salon gift/app/models/servicios.py
@@ -1,24 +1,38 @@
 from app import db
 
+# Tabla intermedia para Servicios y Productos
+servicio_productos = db.Table('servicio_productos',
+    db.Column('servicio_id', db.Integer, db.ForeignKey('servicios.idservicio'), primary_key=True),
+    db.Column('producto_id', db.Integer, db.ForeignKey('productos.idproductos'), primary_key=True)
+)
+
 class Servicio(db.Model): 
     __tablename__ = 'servicios'
     
     idservicio = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), unique=True, nullable=False)
     precio = db.Column(db.Numeric(10, 2), nullable=False) 
-    duracion = db.Column(db.String(250), nullable=False)
+    duracion = db.Column(db.String(250), nullable=False)  
+    categoria = db.Column(db.String(50), nullable=False) 
 
-    def __init__(self, nombre, precio, duracion):
+    idcitas = db.Column(db.Integer, db.ForeignKey('citas.idcitas'), nullable=False, unique=True)
+    citas = db.relationship('Citas', back_populates='servicios')
+    
+    productos = db.relationship('Productos', secondary=servicio_productos, backref='servicios')
+
+    def __init__(self, nombre, precio, duracion, categoria):
         self.nombre = nombre
         self.precio = precio
         self.duracion = duracion
+        self.categoria = categoria
 
     def to_dict(self):
         return {
             "idservicio": self.idservicio,
             "nombre": self.nombre,
             "precio": float(self.precio),
-            "duracion": self.duracion
+            "duracion": self.duracion,
+            "categoria": self.categoria
         }
 
     def save(self):
