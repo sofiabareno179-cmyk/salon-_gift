11e540e4fa66d4b13e5b02d83f48d9242538a66f
dannakatherinecubidesdiaz-max <dannakatherinecubidesdiaz@gmail.com>
2026-05-06 16:59:57 -0500

    Update agenda.py

--- salon gift/app/models/agenda.py
+++ salon gift/app/models/agenda.py
@@ -1,11 +1,18 @@
 from flask_login import UserMixin
 from app import db
+
 class Agenda(db.Model, UserMixin): 
     __tablename__ = 'agenda'
     idagenda = db.Column(db.Integer, primary_key=True)
     diasemana = db.Column(db.String(100), nullable=False) 
     horainicio = db.Column(db.String(100), nullable=False) 
     horafin = db.Column(db.String(250), nullable=False)
+
+    idusuario = db.Column(db.Integer, db.ForeignKey('usuario.idusuario'), nullable=False, unique=True)
+    usuario = db.relationship('User', back_populates='agenda')
+    
+
+
     def __init__(self, diasemana, horainicio, horafin):
         self.diasemana = diasemana
         self.horainicio = horainicio
