5f847ffeb388273e6b765826324ce8336ad56299
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-09-11 19:02:25 -0500

    Puerto 5000 (PORT default y EXPOSE) en vez de 80

--- salon_gift/run.py
+++ salon_gift/run.py
@@ -7,5 +7,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', '80'))
+    port = int(os.environ.get('PORT', '5000'))
     app.run(debug=False, host='0.0.0.0', port=port)
