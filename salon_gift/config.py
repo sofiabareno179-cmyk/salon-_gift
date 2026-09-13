4096731990d89c2293434aeaede7956487095d5a
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-09-13 14:43:47 -0500

    Update config.py

--- salon_gift/config.py
+++ salon_gift/config.py
@@ -9,7 +9,7 @@ class Config:
             _db_url = _db_url.replace('postgresql://', 'postgresql+psycopg://', 1)
         SQLALCHEMY_DATABASE_URI = _db_url
     else:
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg://admin:9027865@service-ja02h7flk87vuyhhd4cvwrza:5432/salonglitt_db'
+        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg://admin:9027865@172.17.0.1:5431/salonglitt_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_urlsafe(24)
     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads', 'servicios')
